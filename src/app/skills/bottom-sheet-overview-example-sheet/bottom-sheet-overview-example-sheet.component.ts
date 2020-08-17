faa3ecfab337a4c883eec9fcca73e405baa3b4d3
Martin Barria Joost <martinjoostw@gmail.com>
2020-08-17 16:49:51 -0600

    chore(http): request firebase json data

--- src/app/skills/bottom-sheet-overview-example-sheet/bottom-sheet-overview-example-sheet.component.ts
+++ src/app/skills/bottom-sheet-overview-example-sheet/bottom-sheet-overview-example-sheet.component.ts
@@ -1,51 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BriefcaseService } from "src/app/services/briefcase.service";
 
 @Component({
-  selector: 'app-bottom-sheet-overview-example-sheet',
-  templateUrl: './bottom-sheet-overview-example-sheet.component.html',
-  styleUrls: ['./bottom-sheet-overview-example-sheet.component.css']
+  selector: "app-bottom-sheet-overview-example-sheet",
+  templateUrl: "./bottom-sheet-overview-example-sheet.component.html",
+  styleUrls: ["./bottom-sheet-overview-example-sheet.component.css"],
 })
 export class BottomSheetOverviewExampleSheetComponent implements OnInit {
+  versions: [] = [];
 
-
-  versions: any = [
-    {
-      name: 'angular 2',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angular 4',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angular 5',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angular 6',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angular 7',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angular 8',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angular 9',
-      href: 'https://angular.io',
-    },
-    {
-      name: 'angularjs',
-      href: 'https://angularjs.org',
-    },
-  ];
-
-  constructor() { }
+  constructor(private briefcaseService: BriefcaseService) {}
 
   ngOnInit() {
+    this.briefcaseService.getSkills().subscribe((resp) => {
+      if (resp) {
+        this.versions = resp.frameworks.find(
+          (framework) => framework.name === "angular"
+        ).versions;
+      }
+    });
   }
-
 }
--- src/app/skills/skills/skills.component.ts
+++ src/app/skills/skills/skills.component.ts
@@ -1,43 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-import { MatBottomSheet, MatBottomSheetRef } from '@angular/material';
-import { BottomSheetOverviewExampleSheetComponent } from '../bottom-sheet-overview-example-sheet/bottom-sheet-overview-example-sheet.component';
-import { BriefcaseService } from 'src/app/services/briefcase.service';
-import { SkillModel } from 'src/app/models/skill.model';
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { MatBottomSheet, MatBottomSheetRef } from "@angular/material";
+import { BottomSheetOverviewExampleSheetComponent } from "../bottom-sheet-overview-example-sheet/bottom-sheet-overview-example-sheet.component";
+import { BriefcaseService } from "src/app/services/briefcase.service";
+import { SkillModel } from "src/app/models/skill.model";
 
 @Component({
-  selector:  'app-skills',
-  templateUrl:  './skills.component.html',
-  styleUrls:  ['./skills.component.css']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.css"],
 })
 export class SkillsComponent implements OnInit {
-  languages:  SkillModel;
+  languages: SkillModel;
   othersLibs: SkillModel;
   frameworks: SkillModel;
   loading = false;
   error: boolean;
-  constructor(private titleService:  Title,
-              private _bottomSheet:  MatBottomSheet,
-              private briefcaseService: BriefcaseService) {}
+  constructor(
+    private titleService: Title,
+    private _bottomSheet: MatBottomSheet,
+    private briefcaseService: BriefcaseService
+  ) {}
 
   ngOnInit() {
     this.loading = true;
-    this.briefcaseService.getSkills().subscribe( resp => {
-      this.loading = false;
-      if (resp) {
-        this.languages = resp.languages;
-        this.othersLibs = resp.othersLibs;
-        this.frameworks = resp.frameworks;
-      } else {
+    this.briefcaseService.getSkills().subscribe(
+      (resp) => {
+        this.loading = false;
+        if (resp) {
+          localStorage.setItem("item", JSON.stringify(resp));
+          this.languages = resp.languages;
+          this.othersLibs = resp.othersLibs;
+          this.frameworks = resp.frameworks;
+        } else {
+          this.error = true;
+        }
+      },
+      (err) => {
         this.error = true;
       }
-    }, err => {
-      this.error = true;
-    });
-    this.titleService.setTitle('Habilidades');
+    );
+    this.titleService.setTitle("Habilidades");
   }
 
-  openBottomSheet():  void {
+  openBottomSheet(): void {
     this._bottomSheet.open(BottomSheetOverviewExampleSheetComponent);
   }
 }
